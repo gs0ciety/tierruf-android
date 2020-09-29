067be93460e53fc9b0740563a3635acdabdae525
Santiago Andracchi <santiago.andracchi@gmail.com>
2020-09-29 17:00:59 -0300

    Remove non needed files

--- app/src/main/java/com/gs0ciety/interfaces/ButtonPanelFragmentInterface.java
+++
@@ -1,5 +0,0 @@
-package com.gs0ciety.interfaces;
-
-public interface ButtonPanelFragmentInterface {
-    void changeLanguage(final String language);
-}
--- app/src/main/java/com/gs0ciety/tierruf/fragment/GameFragment.java
+++ app/src/main/java/com/gs0ciety/tierruf/fragment/GameFragment.java
@@ -1,6 +1,5 @@
 package com.gs0ciety.tierruf.fragment;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -21,7 +20,6 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.airbnb.lottie.LottieAnimationView;
